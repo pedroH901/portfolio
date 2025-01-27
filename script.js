a48a9269930c8387f5e8fadf534a1bef78b5e0c7
pedroH901 <82474105+pedroH901@users.noreply.github.com>
2025-01-27 15:15:51 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,3 +5,32 @@ $(document).ready(function () {
       $(this).addClass("active");
     });
   });
+
+let isDragging = false;
+let startX;
+let scrollLeft;
+
+$(".conteudo").mousedown(function(e) {
+    isDragging = true;
+    startX = e.pageX - $(this).offset().left;
+    scrollLeft = $(this).scrollLeft();
+    $(this).css('cursor', 'grabbing');
+});
+
+$(document).mouseup(function() {
+    isDragging = false;
+    $(".conteudo").css('cursor', 'grab');
+});
+
+$(document).mouseleave(function() {
+    isDragging = false;
+    $(".conteudo").css('cursor', 'grab');
+});
+
+$(".conteudo").mousemove(function(e) {
+    if (!isDragging) return;
+    e.preventDefault();
+    const x = e.pageX - $(this).offset().left;
+    const walk = x - startX;
+    $(this).scrollLeft(scrollLeft - walk);
+});
